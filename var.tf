4d8c11e53c0b0ba7b2658910d1d8cea2d2b29fee
harishdivvela <harishdivvela961@gmail.com>
2021-08-19 14:44:58 +0530

    newfile
    var.tf

---
+++ var.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  type = string
+  default = "us-east-2"
+}
+variable "ami" {
+ type = string
+ default = "ami-035287fe01e89ce94"
+}
+
+variable "int_type" {
+  type = string
+  default = "t2.xlarge"
+}
+
+#variable "subnet" {
+#  type = string
+#  default = "subnet-08bc8bc52fdf01323"
+  
+#}
+  
+variable "bucket" {
+   type = string
+   default = "my-bucket-03-08-2022"
+}
+
+variable "key" {
+ #  type = list
+   type = string
+   default = "test"
+}
+#variable "s3_folders" {
+#   type        = list
+#   description = "The list of S3 folders to create"
+#   default     = "s3-folders"
+#}
